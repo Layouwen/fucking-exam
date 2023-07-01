b1a2406be633ed40dfbbcdfa62ae92809d4b050e
Avan <layouwen@gmail.com>
2023-07-01 11:28:44 +0800

    chore: remove eslint

--- packages/client-frontend/nuxt.config.ts
+++ packages/client-frontend/nuxt.config.ts
@@ -1,7 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    "@nuxtjs/eslint-module",
     "@nuxtjs/tailwindcss",
     "@pinia/nuxt",
     "@pinia-plugin-persistedstate/nuxt",
