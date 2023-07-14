74b536be0a036fca9af8c3856a7a64d2dfdad2bc
Avan <layouwen@gmail.com>
2023-07-14 19:27:19 +0800

    feat: add Header global component

---
+++ packages/client-frontend/components/global/Header.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="h-[50px] w-full flex items-center fixed top-0 left-0 bg-[#fff] z-10">
+    <div class="w-[70px] text-center">
+      <span v-if="!isHome" @click="onGoHome">主页</span>
+    </div>
+    <span class="flex-grow text-center">问卷列表</span>
+    <div class="w-[70px] text-center">
+      <span v-if="isLogin()">我的</span>
+      <span v-else @click="onOpenOptionModal">登录</span>
+    </div>
+  </div>
+  <van-action-sheet v-model:show="showModal" :actions="actions" @select="onSelect" close-on-click-action />
+</template>
+
+<script lang="ts" setup>
+import { isLogin } from '@fucking-exam/shared'
+import config from '~/config'
+
+const isHome = computed(() => window.location.pathname === '/')
+
+const actions = [{ name: '邮箱登录' }, { name: 'Github登录' }, { name: '注册' }, { name: '取消' }]
+
+const onSelect = (item: { name: string }) => {
+  switch (item.name) {
+    case 'Github登录':
+      window.location.href = `https://github.com/login/oauth/authorize?client_id=${config.github.clientId}&redirect_uri=${window.location.origin}/oauth/github&scope=user&response_type=token`
+      return
+    case '邮箱登录':
+      return navigateTo('/login')
+    case '注册':
+      return navigateTo('/register')
+  }
+}
+
+const onGoHome = () => {
+  navigateTo('/')
+}
+
+const showModal = ref(false)
+const onOpenOptionModal = () => {
+  showModal.value = true
+}
+</script>
--- packages/client-frontend/pages/index.vue
+++ packages/client-frontend/pages/index.vue
@@ -1,14 +1,6 @@
 <template>
-  <van-action-sheet
-    v-model:show="showModal"
-    :actions="actions"
-    @select="onSelect"
-    close-on-click-action
-  />
-  <div class="min-h-[100vh] flex flex-col">
-    <div class="text-[26px] flex justify-center items-center py-4">
-      问卷列表
-    </div>
+  <Header />
+  <div class="min-h-[calc(100vh-50px)] mt-[50px] flex flex-col">
     <div class="px-4 pt-4 flex-grow">
       <n-card
         class="mb-4"
@@ -17,13 +9,10 @@
         :title="i.paperName"
         @click="onClickQuestionnaire(i.id)"
       >
-        创建日期: {{ dayjs(i.createdAt).format("YYYY-MM-DD HH:mm:ss") }}
+        创建日期: {{ dayjs(i.createdAt).format('YYYY-MM-DD HH:mm:ss') }}
       </n-card>
     </div>
-    <div
-      class="bg-[#f9f9f9] text-[14px] py-4 flex items-center justify-center"
-      @click="onSupport"
-    >
+    <div class="bg-[#f9f9f9] text-[14px] py-4 flex items-center justify-center" @click="onSupport">
       <span class="text-[#8c8c8c] mr-2">Avan</span>
       <span class="text-[#bebebe]">提供技术支持</span>
     </div>
@@ -31,57 +20,28 @@
 </template>
 
 <script lang="ts" setup>
-import { NCard, useMessage } from "naive-ui";
-import { getQuestionnaireListApi } from "~/api";
-import { onMounted, ref } from "vue";
-import { useRouter } from "vue-router";
-import dayjs from "dayjs";
-import { isMobile, isLogin } from "@fucking-exam/shared";
-import config from "~/config";
+import { NCard, useMessage } from 'naive-ui'
+import { getQuestionnaireListApi } from '~/api'
+import { onMounted, ref } from 'vue'
+import dayjs from 'dayjs'
+import { isMobile } from '@fucking-exam/shared'
 
-const message = useMessage();
-const router = useRouter();
-
-const actions = [
-  { name: "邮箱登录" },
-  { name: "Github登录" },
-  { name: "注册" },
-  { name: "取消" },
-];
-
-const showModal = ref(false);
-const questionnaireList = ref<any>([]);
+const message = useMessage()
+const questionnaireList = ref<any>([])
 
 onMounted(async () => {
-  const listRes = await getQuestionnaireListApi();
-  questionnaireList.value = listRes.data.list;
-});
-
-const onSelect = (item: { name: string }) => {
-  switch (item.name) {
-    case "Github登录":
-      window.location.href = `https://github.com/login/oauth/authorize?client_id=${config.github.clientId}&redirect_uri=${window.location.origin}/oauth/github&scope=user&response_type=token`;
-      return;
-    case "邮箱登录":
-      return router.push("/login");
-    case "注册":
-      return router.push("/register");
-  }
-};
+  const listRes = await getQuestionnaireListApi()
+  questionnaireList.value = listRes.data.list
+})
 
 const onClickQuestionnaire = (id: number) => {
-  if (isLogin()) {
-    router.push(`/questionnaire/${id}`);
-  } else {
-    showModal.value = true;
-  }
-};
+  navigateTo(`/questionnaire/${id}`)
+}
 
 const onSupport = () => {
   if (isMobile()) {
-    message.info("打开QQ");
-    window.location.href =
-      "mqq://im/chat?chat_type=wpa&uin=794234293&version=1&src_type=web";
+    message.info('打开QQ')
+    window.location.href = 'mqq://im/chat?chat_type=wpa&uin=794234293&version=1&src_type=web'
   }
-};
+}
 </script>
