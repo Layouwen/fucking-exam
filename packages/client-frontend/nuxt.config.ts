284a86bc9a0f172dc5d8fc0ba4a0f2b7440bc252
Avan <layouwen@gmail.com>
2023-08-23 17:01:40 +0800

    chore: introducing tdesign-vue-next ui

--- packages/client-frontend/nuxt.config.ts
+++ packages/client-frontend/nuxt.config.ts
@@ -1,52 +1,42 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    "@nuxtjs/tailwindcss",
-    "@pinia/nuxt",
-    "@pinia-plugin-persistedstate/nuxt",
-    "@nuxtjs/color-mode",
-    "nuxt-icon",
-    "nuxt-lodash",
-    "nuxt-vitest",
-    "@vant/nuxt",
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
+    '@pinia-plugin-persistedstate/nuxt',
+    '@nuxtjs/color-mode',
+    'nuxt-icon',
+    'nuxt-lodash',
+    'nuxt-vitest',
+    '@vant/nuxt',
+    'tdesign-vue-next/es/nuxt',
   ],
   pinia: {
     autoImports: [
       // automatically imports `defineStore`
-      "defineStore", // import { defineStore } from 'pinia'
-      ["defineStore", "definePiniaStore"], // import { defineStore as definePiniaStore } from 'pinia'
+      'defineStore', // import { defineStore } from 'pinia'
+      ['defineStore', 'definePiniaStore'], // import { defineStore as definePiniaStore } from 'pinia'
     ],
   },
   // @ts-ignore
   build: {
     transpile:
-      process.env.NODE_ENV === "production"
-        ? [
-            "naive-ui",
-            "vueuc",
-            "@css-render/vue3-ssr",
-            "@juggle/resize-observer",
-          ]
-        : ["@juggle/resize-observer"],
+      process.env.NODE_ENV === 'production'
+        ? ['naive-ui', 'vueuc', '@css-render/vue3-ssr', '@juggle/resize-observer', 'tdesign-vue-next']
+        : ['@juggle/resize-observer', 'tdesign-vue-next'],
   },
   vite: {
     optimizeDeps: {
-      include:
-        process.env.NODE_ENV === "development"
-          ? ["naive-ui", "vueuc", "date-fns-tz/esm/formatInTimeZone"]
-          : [],
+      include: process.env.NODE_ENV === 'development' ? ['naive-ui', 'vueuc', 'date-fns-tz/esm/formatInTimeZone'] : [],
     },
     server: {
       proxy: {
-        "/api": {
-          target:
-            process.env.VITE_PROXY_TYPE === "local"
-              ? "http://localhost:3000"
-              : "http://easyhappy.top",
+        '/api': {
+          target: process.env.VITE_PROXY_TYPE === 'local' ? 'http://localhost:3000' : 'http://easyhappy.top',
           changeOrigin: true,
           // rewrite: (path: string) => path.replace(/^\/api/, ""),
         },
       },
     },
   },
-});
+})
