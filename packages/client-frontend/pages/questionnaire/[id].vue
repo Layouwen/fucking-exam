8e6d9f15c25a4c3fa21318e10dac7124c0f32414
Avan <layouwen@gmail.com>
2023-08-24 16:04:03 +0800

    refactor: remove all naive components

--- packages/client-frontend/pages/questionnaire/[id].vue
+++ packages/client-frontend/pages/questionnaire/[id].vue
@@ -1,38 +1,62 @@
 <template>
   <div v-if="data" class="pt-[16px]">
-    <n-progress
-      class="fixed top-0 left-0 w-full z-10"
-      :indicator-placement="'inside'"
-      type="line"
+    <t-progress
+      class="custom-progress fixed top-0 left-0 w-full z-10"
       :percentage="percentage"
-      :height="16"
-      :border-radius="0"
-      :fill-border-radius="0"
+      theme="plump"
+      size="small"
     />
     <questionnaire-render :data="data" :result="answers" />
     <div class="px-6 pb-6">
-      <NButton block type="info" @click="onSubmit">提交试卷</NButton>
-      <NButton class="mt-2" block @click="router.push('/')">返回</NButton>
+      <van-button size="small" type="primary" block @click="onSubmit">提交试卷</van-button>
+      <van-button size="small" block class="!mt-2" @click="router.push('/')">返回</van-button>
     </div>
   </div>
   <div v-else class="flex flex-col min-h-[100vh] items-center">
-    <n-skeleton class="mt-[30px]" text height="32px" width="50%" />
+    <t-skeleton
+      class="!mt-[30px] w-full"
+      :rowCol="[
+        {
+          height: 32,
+          width: '50%',
+          marginLeft: '25%',
+        },
+      ]"
+      animation="gradient"
+    />
     <template v-for="i in 2" :key="i">
-      <div class="mt-[30px] w-[80%]">
-        <n-skeleton text height="26px" width="70%" />
+      <div class="mt-[30px] w-full">
+        <t-skeleton
+          class="w-full"
+          :rowCol="[
+            {
+              height: 26,
+              width: '60%',
+              marginLeft: '10%',
+            },
+          ]"
+          animation="gradient"
+        />
+        <t-skeleton
+          v-for="mt in [10, 6, 6, 6]"
+          class="w-full"
+          :style="{ marginTop: `${mt}px` }"
+          :rowCol="[
+            {
+              height: 46,
+              width: '80%',
+              marginLeft: '10%',
+            },
+          ]"
+          animation="gradient"
+        />
       </div>
-      <n-skeleton class="mt-[10px]" height="46px" width="80%" />
-      <n-skeleton class="mt-[6px]" height="46px" width="80%" />
-      <n-skeleton class="mt-[6px]" height="46px" width="80%" />
-      <n-skeleton class="mt-[6px]" height="46px" width="80%" />
     </template>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { Questionnaire, QuestionnaireClientSubmitRequestBody, isLogin, randomByArr } from '@fucking-exam/shared'
-// TODO 移除 naive-ui
-import { NButton, NProgress, NSkeleton } from 'naive-ui'
 import { showFailToast } from 'vant'
 import { onMounted, computed } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
@@ -106,3 +130,14 @@ const onSubmit = async () => {
   }
 }
 </script>
+
+<style scoped lang="less">
+.custom-progress {
+  :deep(.t-progress--plump) {
+    border-radius: 0 !important;
+    .t-progress__inner {
+      border-radius: 0 !important;
+    }
+  }
+}
+</style>
--- packages/client-frontend/pages/questionnaire/response/[id].vue
+++ packages/client-frontend/pages/questionnaire/response/[id].vue
@@ -5,36 +5,36 @@
     :answers="answers"
     :result="answers"
   />
-  <n-button block type="primary" @click="router.push('/')">返回</n-button>
+  <div class="px-6 pb-6">
+    <van-button size="small" type="primary" block @click="router.push('/')">返回</van-button>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import { getQuestionnaireResponseApi } from "~/api";
-import { QuestionnaireRender, QuestionnaireRenderType } from "~/components";
-import { useRoute, useRouter } from "vue-router";
-import { onMounted, ref } from "vue";
-// TODO 移除 naive-ui
-import { NButton } from "naive-ui";
+import { getQuestionnaireResponseApi } from '~/api'
+import { QuestionnaireRender, QuestionnaireRenderType } from '~/components'
+import { useRoute, useRouter } from 'vue-router'
+import { onMounted, ref } from 'vue'
 
-const route = useRoute();
-const router = useRouter();
+const route = useRoute()
+const router = useRouter()
 
-const data = ref<Record<string, any>>();
-const answers = ref<Record<string, string[]>>();
+const data = ref<Record<string, any>>()
+const answers = ref<Record<string, string[]>>()
 
 onMounted(async () => {
-  const { id } = route.params as { id: string };
-  const res = await getQuestionnaireResponseApi(id);
+  const { id } = route.params as { id: string }
+  const res = await getQuestionnaireResponseApi(id)
   if (res.code === 200) {
     const {
       data: { questions, questionnaire, answers: _answers },
-    } = res;
-    answers.value = _answers;
+    } = res
+    answers.value = _answers
     data.value = {
       paperName: questionnaire.paperName,
       questions,
       settings: questionnaire.settings,
-    };
+    }
   }
-});
+})
 </script>
